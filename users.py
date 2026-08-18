d16d869449e4aa68affd12230a820b264cddc555
RosiPosi <116714787+RosiPosi@users.noreply.github.com>
2026-08-18 23:37:53 +0300

    Users post count

--- users.py
+++ users.py
@@ -7,12 +7,21 @@ def get_user(user_id):
     result = db.query(sql, [user_id])
     return result[0] if result else None
 
-def get_item(user_id):
+def get_item(user_id, page=1):
+    limit = 15
+    offset = (page - 1) * limit
     sql = """SELECT id, title
-            FROM items
-            WHERE user_id = ?
-            ORDER BY id DESC"""
-    return db.query(sql, [user_id])
+             FROM items
+             WHERE user_id = ?
+             ORDER BY id DESC
+             LIMIT ? OFFSET ?"""
+    return db.query(sql, [user_id, limit, offset])
+
+def get_item_count(user_id):
+    sql = "SELECT COUNT(*) FROM items WHERE user_id = ?"
+    result = db.query(sql, [user_id])
+
+    return result[0][0]
 
 def create_user(username, password):
     password_hash = generate_password_hash(password)
